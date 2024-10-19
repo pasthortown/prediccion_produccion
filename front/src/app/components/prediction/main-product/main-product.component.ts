f28e0f0270a8c646c6910456eb1f0761f696383e
Luis Alfonso Salazar Vaca <luis.salazar@kfc.com.ec>
2024-10-19 10:46:36 -0500

    fluidez

--- front/src/app/components/prediction/main-product/main-product.component.ts
+++ front/src/app/components/prediction/main-product/main-product.component.ts
@@ -20,13 +20,19 @@ export class MainProductComponent implements OnChanges {
 
   updateTimes() {
     const minutos = this.tiempo.getMinutes();
-    const intervalo = minutos % 10;
-    if (intervalo <= 3) {
-      this.preparando_time = (intervalo / 3) * 100;
+    const segundos = this.tiempo.getSeconds();
+    const inicioIntervalo = Math.floor(minutos / 10) * 10;
+    const intervalo = minutos - inicioIntervalo;
+    const fraccionSegundos = segundos / 60;
+    if (intervalo >= 0 && intervalo <= 3) {
+      const tiempoTotal = intervalo + fraccionSegundos;
+      this.preparando_time = (tiempoTotal / 3) * 100;
       this.retencion_time = 0;
-    } else {
+    }
+    else if (intervalo > 3 && intervalo <= 9) {
+      const tiempoTotal = (intervalo - 3) + fraccionSegundos;
       this.preparando_time = 100;
-      this.retencion_time = ((intervalo - 3) / 7) * 100;
+      this.retencion_time = (tiempoTotal / 6) * 100;
     }
   }
 }
